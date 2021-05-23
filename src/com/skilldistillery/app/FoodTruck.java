873079575fa0d5b443ef430898422782defe42a9
ren09 <rachel.richardson915@gmail.com>
2021-05-23 15:48:07 -0600

    saving progress

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,5 +1,80 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
+	
+	private FoodTruck[] foodTrucks;
+
+	private String nameOfTruck;
+	private String foodType;
+	private int rating;
+	private static int truckId = 0;
+	private int newId;
+
+	public FoodTruck(String nameOfTruck, String foodType, int rating, int truckId) {
+		this.nameOfTruck = nameOfTruck;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.newId = truckId;
+	}
+
+	public String getNameOfTruck() {
+		return nameOfTruck;
+	}
+
+	public void setNameOfTruck(String nameOfTruck) {
+		this.nameOfTruck = nameOfTruck;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getTruckId(int id) {
+		return newId;
+	}
+	
+	public void setTruckId(FoodTruck foodTruck) {
+		foodTrucks[FoodTruck.truckId] = foodTruck;
+		FoodTruck.truckId++;
+		this.newId = FoodTruck.truckId;
+	}
+//		newTruckId = 0;
+//		for(int i =0; i<newTruckId; i++) {
+//			FoodTruck.truckId = newTruckId;
+//		}
+//	}
+
+//		FoodTruck a = new FoodTruck(nameOfTruck, foodType, rating, truckId);
+//		for (int i = 0; i < a.numOfTrucks; i++) {
+//			int truckId = i;
+//			truckId++;
+//			FoodTruck.truckId = truckId;
+//			return FoodTruck.truckId;
+//		}
+//	}
+
+
+	@Override
+	public String toString() {
+		return "FoodTruck [nameOfTruck= " + nameOfTruck + ", foodType= " + foodType + ", rating= " + rating
+				+ ", truckId= " + FoodTruck.truckId + "]";
+	}
+
+	public void displayTruck() {
+		String truckInfo = this.toString();
+		System.out.println(truckInfo);
+	}
 
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,39 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
-
+ 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		FoodTruckApp foodTruckApp = new FoodTruckApp();
+		
+		FoodTruck[] foodTrucks = new FoodTruck[5];
+		for (int i = 0; i < foodTrucks.length; i++) {
+			Scanner input = new Scanner(System.in);
+			
+			System.out.print("Enter the food truck's name: ");
+			String truckName = input.nextLine();
+			
+			if(truckName.equalsIgnoreCase("quit")) {
+				foodTruckApp.menu(foodTrucks);
+			}
+			System.out.print("Enter the type of food served: ");
+			String foodType = input.nextLine();
+			
+			System.out.print("Enter the food truck's rating: ");
+			int rating = input.nextInt();
+			
+			int truckId = foodTrucks[i].getTruckId(i);
+			
+			foodTrucks[i] = new FoodTruck(truckName, foodType, rating, truckId);
+		}
 	}
-
-}
+	
+	public void menu(FoodTruck[] foodTrucks) {
+		for(int i=0; i<foodTrucks.length; i++) {
+			foodTrucks[i].displayTruck();
+		}
+		
+	}
+}			
